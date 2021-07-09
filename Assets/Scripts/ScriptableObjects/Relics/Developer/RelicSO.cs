d70660342e94b543f749d2b2c782aadd3b7927bc
Rei <reisegev@yahoo.com>
2021-07-09 23:48:34 +0300

    editor

--- Assets/Scripts/ScriptableObjects/Relics/Developer/RelicSO.cs
+++ Assets/Scripts/ScriptableObjects/Relics/Developer/RelicSO.cs
@@ -1,7 +1,5 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
-using Keywords;
-
+using Sirenix.OdinInspector;
 
 namespace Relics
 {
@@ -12,52 +10,72 @@ namespace Relics
         PowerUp = 2,
         Breath =3
     };
-    public enum RelicType { Passive,Active };
+
     [CreateAssetMenu (fileName = "Relic", menuName = "ScriptableObjects/Relic")]
     public class RelicSO : ScriptableObject
     {
+
         #region Fields
+        //[HorizontalGroup("Basic Info", 100)]
+        //[Space(60)]
+
+        [TitleGroup("Recipe")]
+        [TabGroup("Recipe/General Info","Picture")]
+
+        [PreviewField(100,ObjectFieldAlignment.Center)]
+        [HideLabel]
+        [Tooltip("Recipe's Icon")]
+        [SerializeField] Sprite _icon;
+
+
+        [TabGroup("Recipe/General Info","Data")]
+        [LabelWidth(130)]
         [Tooltip("Relic Name")]
         [SerializeField] RelicNameEnum _relicName;
 
-        [Tooltip("Relic Type ")]
-        [SerializeField] RelicType _relicType;
 
-        [Tooltip("When is it activated?")]
-        [SerializeField] WhenActivatedEnum _whenToActivate;
-
-       // need to enter animation class;
-
+        [TabGroup("Recipe/General Info", "Data")]
         [Tooltip("Relic Rarity")]
+        [LabelWidth(130)]
         [SerializeField] Cards.RarityEnum _rarityLevel;
 
-        [Tooltip("Relic's Icon")]
-        [SerializeField] Image _icon;
 
-        [Tooltip("Relic's Description")]
+        [TabGroup("Recipe/General Info", "Data")]
+        [LabelWidth(100)]
+        [SerializeField] int _cost;
+
+
+
+
+        [TabGroup("Recipe/General Info", "Data")]
+        [Tooltip("Combo Description")]
         [TextArea]
         [SerializeField] string _description;
 
-        [Space]
+
+        [TabGroup("Recipe/General Info", "Combo")]
         [Header("Combo:")]
         [Tooltip("The Order will define the accomplishment of the combo")]
+        [LabelWidth(20)]
         [SerializeField] Cards.BodyPartEnum[] _bodyPart;
 
-
-        [Header("Keywords")]
-        [Tooltip("Card's Keywords:")]
-        [SerializeField] KeywordData[] _keywordEffect;
+      
+        [Space(100)]
+        [BoxGroup]
+        [InlineEditor]
+        [SerializeField] Cards.CardSO _craftedCard;
 
         #endregion
+
         #region Properties
-        public RelicNameEnum GetRelicName => _relicName;
         public Cards.BodyPartEnum[] GetCombo => _bodyPart;
         public Cards.RarityEnum GetRarityEnum => _rarityLevel;
+        public Sprite GetIcon => _icon;
+        public int Cost => _cost;
+        public RelicNameEnum GetRelicName => _relicName;
         public string GetDescription => _description;
-        public Image GetIcon => _icon;
-        public WhenActivatedEnum GetWhenToActivate => _whenToActivate;
-        public KeywordData[] GetKeywordEffect => _keywordEffect;
-        public RelicType GetRelicType => _relicType;
+
+  
         #endregion
 
     }
