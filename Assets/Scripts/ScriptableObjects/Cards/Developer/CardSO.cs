ead726bb473d13e5afde646874b3a8c8fc10f509
Rei <reisegev@yahoo.com>
2021-07-10 23:45:48 +0300

    Card equality

--- Assets/Scripts/ScriptableObjects/Cards/Developer/CardSO.cs
+++ Assets/Scripts/ScriptableObjects/Cards/Developer/CardSO.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using Keywords;
+using System.Collections.Generic;
+
 namespace Cards
 {
     [CreateAssetMenu(fileName = "CardData", menuName = "ScriptableObjects/Cards")]
@@ -12,17 +14,12 @@ namespace Cards
         [Tooltip("Name of the card\n Note: the enum is also used to detect the currect animation")]
         [SerializeField] CardNamesEnum _cardName;
 
-        [Tooltip("Card rarity")]
-        public RarityEnum _rarityLevel;
-
-        [Tooltip("What Type Of Card Is It?")]
-        [SerializeField] CardTypeEnum _cardType;
-
-        [Tooltip("What Section It Is Targeting")]
+        [Tooltip("What Body Part Is Targeting")]
         [SerializeField] TargetedPartEnum _targetBodyPart;
 
-        [Tooltip("What Body Part It Uses")]
-        [SerializeField] BodyPartEnum _bodyPart;
+        [Tooltip("What Type Of Card Is It?")]
+        [SerializeField]
+        CardType _cardData;
 
 
         [Tooltip("The Image of the card:")]
@@ -31,15 +28,15 @@ namespace Cards
 
         [Tooltip("The Description of the card:")]
         [TextArea()]
-        [SerializeField] string _cardDescription;   
+        [SerializeField] string _cardDescription;
         [Tooltip("The Description of the card's LCE:")]
         [SerializeField] string _cardLCEDescription;
 
         [Space]
         [Header("Mana:")]
         [Tooltip("How much stamina the card cost")]
-        [SerializeField] int _staminaCost= 1;
-        
+        [SerializeField] int _staminaCost = 1;
+
 
         [Tooltip("How Many Times This Card Can Be Upgraded")]
         [SerializeField] int _maxUpgradeLevel = 1;
@@ -56,16 +53,17 @@ namespace Cards
         #endregion
 
         #region Properties
-        public RarityEnum GetCardsRarityLevel => _rarityLevel;
+        public CardType GetCardType => _cardData;
+        public RarityEnum GetCardsRarityLevel => _cardData._rarityLevel;
         public CardNamesEnum GetCardName => _cardName;
         public ref Sprite GetCardImage => ref _cardImage;
         public ref string GetCardDescription => ref _cardDescription;
         public ref string GetCardLCEDescription => ref _cardLCEDescription;
-        public CardTypeEnum GetCardTypeEnum => _cardType;
+        public CardTypeEnum GetCardTypeEnum => _cardData._cardType;
         public TargetedPartEnum GetTargetBodyPart => _targetBodyPart;
-        public BodyPartEnum GetBodyPartEnum => _bodyPart;
-        public  int GetStaminaCost =>  _staminaCost;
-        public KeywordData[] GetCardsKeywords =>  _keywords;
+        public BodyPartEnum GetBodyPartEnum => _cardData._bodyPart;
+        public int GetStaminaCost => _staminaCost;
+        public KeywordData[] GetCardsKeywords => _keywords;
         public KeywordData[] GetAdditionalKeywords => _upgrateKeywords;
         public ref int GetKeyWordMaxLevel => ref _maxUpgradeLevel;
         public ref int GetCardLevelToUnlockKeywords => ref _whenUnlockNewKeywords;
@@ -74,11 +72,68 @@ namespace Cards
 
     }
 
-    public enum RarityEnum {
-    Common,
-    Uncommon,
-    Rare,
-    Epic,
-    LegendREI
+    public enum RarityEnum
+    {
+        Common,
+        Uncommon,
+        Rare,
+        Epic,
+        LegendREI
     };
+
+    [System.Serializable]
+    public class CardType
+    {
+        [Tooltip("What Type Of Card Is It?")]
+        public CardTypeEnum _cardType;
+
+        [Tooltip("What Body Part It Uses")]
+        public BodyPartEnum _bodyPart;
+
+        [Tooltip("Card rarity")]
+        public RarityEnum _rarityLevel;
+    }
+
+    public class CardTypeComparaer : IEqualityComparer<CardType>
+    {
+
+        // Products are equal if their names and product numbers are equal.
+        public bool Equals(CardType x, CardType y)
+        {
+
+
+            //Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(x, y))
+                return true;
+
+
+            //Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+
+
+
+            //Check whether the products' properties are equal.
+            return x._bodyPart == y._bodyPart && x._cardType == y._cardType;
+
+        }
+
+
+        // If Equals() returns true for a pair of objects
+        // then GetHashCode() must return the same value for these objects.
+        public int GetHashCode(CardType obj)
+        {
+            //Check whether the object is null
+            if (Object.ReferenceEquals(obj, null))
+                return 0;
+
+            //Get hash code for the Name field if it is not null.
+            int hashBodyPart = obj._bodyPart.GetHashCode();
+            int hashCardType = obj._cardType.GetHashCode();
+            int hash_rarityLevel = obj._rarityLevel.GetHashCode();
+
+            return hashBodyPart ^ hashCardType ^ hash_rarityLevel;
+        }
+
+    }
 }
