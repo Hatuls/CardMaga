a3aa2c416a66e6b3ec343ca156b78d37609b3d27
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-10-23 13:57:45 +0300

    characters

--- Assets/Scripts/Account/Characters/CharacterData.cs
+++ Assets/Scripts/Account/Characters/CharacterData.cs
@@ -1,8 +1,34 @@
-﻿using UnityEngine;
-
+﻿using Characters.Stats;
+using System;
 namespace Account.GeneralData
 {
     public class CharacterData
     {
+    #region Field
+        CharacterEnum _character;
+        CharacterStats _stats;
+        AccountDeck[] _decks;
+        CombosAccountInfo[] _characterCombos;
+        ushort _unlockAtLevel;
+
+    #endregion
+    #region Properties
+        public CharacterEnum Character => _character;
+        public CharacterStats Stats => _stats;
+        public AccountDeck[] Decks => _decks;
+        public CombosAccountInfo[] CharacterCombos => _characterCombos;
+        public ushort unlockAtLevel => _unlockAtLevel;
+        #endregion
+        #region PublicMethods
+        public AccountDeck GetDeckAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+        public void CharacterAccount(CharacterEnum character, CharacterStats stats, AccountDeck[] decks,
+            CombosAccountInfo[] combos, ushort unlocksAtLevel)
+        {
+
+        }
+        #endregion
     }
 }
