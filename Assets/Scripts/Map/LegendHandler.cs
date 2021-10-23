a88d15d18ede566baf049362fd5b22bfe1f42eef
Rei <reisegev@yahoo.com>
2021-10-23 11:53:50 +0300

    d

---
+++ Assets/Scripts/Map/LegendHandler.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+namespace Meta.Map
+{
+
+    public class LegendHandler : MonoBehaviour
+    {
+        [SerializeField] GameObject _legendPanel;
+        [SerializeField] Animator _legendAnimator;
+        [SerializeField] GameObject _legendInfoContainer;
+
+        int _entranceAnimatioHash = Animator.StringToHash("Entrance");
+        int _exitAnimationHash = Animator.StringToHash("Exit");
+
+        private void Start()
+        {
+            _legendInfoContainer.SetActive(false);
+        }
+
+
+        public void OnLegendButtonPress()
+        {
+            if (_legendInfoContainer.activeSelf)
+                CloseLegend();
+            else
+                OpenLegend();
+        }
+        private void OpenLegend()
+        {
+            SetLegendIfoContainerState(true);
+            _legendAnimator.Play(_entranceAnimatioHash);
+        }
+        private void CloseLegend()
+        {
+            _legendAnimator.Play(_exitAnimationHash);
+        }
+        public void SetLegendIfoContainerState(bool state)
+            => _legendInfoContainer.SetActive(state);
+    }
+
+}
