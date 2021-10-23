a2794d5d41f32251e0408a28cf6c33bc94946cff
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-10-23 23:11:30 +0300

    NewClasses

---
+++ Assets/Scripts/Account/Settings/AccountSettingsData.cs
@@ -0,0 +1,22 @@
+﻿namespace Account.Settings
+{
+    public class AccountSettingsData
+    {
+        #region Fields
+        bool _musicSwitch;
+        bool _sfxSwitch;
+        bool _screenShake;
+        #endregion
+        #region Properties
+        public bool MusicSwitch { get => _musicSwitch; set => _musicSwitch = value; }
+        public bool SFXSwitch { get => _sfxSwitch; set => _sfxSwitch = value; }
+        public bool ScreenShake { get => _screenShake; set => _screenShake = value; }
+        #endregion
+        #region PublicMethods
+        public void InitData(AccountSettingsData accountSettingsData = null)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Meta Game/Server/DealData.cs
@@ -0,0 +1,15 @@
+﻿namespace Server
+{
+    public class DealData
+    {
+        #region Fields
+        DealSO[] _deals;
+        #endregion
+        #region Public Methods
+        public void Init()
+        {
+
+        }
+        #endregion
+    }
+}
