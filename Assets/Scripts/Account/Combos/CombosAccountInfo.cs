343ffb2a7fbfb31f76e5e93dc14f35a54591658a
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-10-25 02:23:57 +0300

    AccountCharacters

--- Assets/Scripts/Account/Combos/CombosAccountInfo.cs
+++ Assets/Scripts/Account/Combos/CombosAccountInfo.cs
@@ -10,7 +10,13 @@
         public ushort ID => _id;
         public byte Level => _level;
         #endregion
-
+        #region Public Methods
+        public CombosAccountInfo(ushort id, byte level)
+        {
+            _id = id;
+            _level = level;
+        }
         public void LevelUp() => _level++;
+        #endregion
     }
 }
