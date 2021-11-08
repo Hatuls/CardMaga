bbd1b89eaf2a9bdc0916406077972586a5051681
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-11-08 17:07:01 +0200

    UI Droplist

---
+++ Assets/Scripts/Meta Game/Cards/MetaCardUIHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetaCardUIHandler : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _dropList;
+    public void OnCardClicked()
+    {
+        //There can be only one open Droplist from all of the cards...
+        //any action other than pressing a button on the drop list will be closeing the drop list
+        //Can be a static class?
+        Debug.Log("Changeing Drop List State");
+        if(_dropList.activeSelf)
+        {
+            _dropList.SetActive(false);
+        }
+        else
+        {
+            _dropList.SetActive(true);
+        }
+    }
+}
