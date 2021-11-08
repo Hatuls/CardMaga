de0b49fe4d04c339ab0bf5e5bcac503e63f22f90
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-11-08 19:43:37 +0200

    Card Enum Implemented with buttons

--- Assets/Scripts/Meta Game/Cards/MetaCardUIHandler.cs
+++ Assets/Scripts/Meta Game/Cards/MetaCardUIHandler.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 
 namespace UI.Meta.Laboratory
 {
+    enum CardPositionEnum
+    {
+        CardCollection = 0,
+        Deck = 1,
+        Fuse = 2,
+        Upgrade = 3,
+    }
+
     public class MetaCardUIHandler : MonoBehaviour
     {
         public static event Action OnCardClickedEvent;
         [SerializeField]
         GameObject _dropList;
+        [SerializeField]
+        GameObject _useButton;
+        [SerializeField]
+        GameObject _dismantleButton;
+        [SerializeField]
+        CardPositionEnum _currentPosition;
 
         private void Awake()
         {
@@ -18,11 +32,28 @@ namespace UI.Meta.Laboratory
         public void CloseDropList()
         {
             _dropList.SetActive(false);
+            _useButton.SetActive(false);
+            _dismantleButton.SetActive(false);
 
         }
         public void OpenDropList()
         {
             _dropList.SetActive(true);
+            switch (_currentPosition)
+            {
+                case CardPositionEnum.CardCollection:
+                    _useButton.SetActive(true);
+                    break;
+                case CardPositionEnum.Deck:
+                    break;
+                case CardPositionEnum.Fuse:
+                    break;
+                case CardPositionEnum.Upgrade:
+                    _dismantleButton.SetActive(true);
+                    break;
+                default:
+                    break;
+            }
         }
         public void OnCardClicked()
         {
