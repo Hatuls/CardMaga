d09ed3d63c8ff752c0c3187362346a3dbe288f49
Rei <reisegev@yahoo.com>
2021-11-14 12:06:41 +0200

    observer

--- Assets/Scripts/DesignPatterns/Observer.cs
+++ Assets/Scripts/DesignPatterns/Observer.cs
@@ -1,24 +1,75 @@
-﻿
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
+
 namespace DesignPattern
 {
 
-    [CreateAssetMenu(fileName = "Observer", menuName = "ScriptableObjects/DesignPatterns/Observer")]
-    public class Observer : ScriptableObject
-    {
 
+
+
+    public class Observer : ISubject
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+        public void Notify(IObserver theOneWhoDidIt)
+        {
+            for (int i = 0; i < _observers.Count; i++)
+                _observers[i].OnNotify(theOneWhoDidIt);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            if (!_observers.Contains(observer))
+                _observers.Add(observer);
+        }
+
+        public void UnSubscribe(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+    }
+
+    public class Observer<T> : ISubject<T>
+    {
+        private List<IObserver<T>> _observers = new List<IObserver<T>>();
+        public void Notify(T data)
+        {
+            for (int i = 0; i < _observers.Count; i++)
+                _observers[i].OnNotify(data);
+
+        }
+
+        public void Subscribe(IObserver<T> observer)
+        {
+            if (!_observers.Contains(observer))
+                _observers.Add(observer);
+        }
+
+        public void UnSubscribe(IObserver<T> observer)
+        {
+            _observers.Remove(observer);
+        }
+    }
+
+
+    public interface IObserver<T>
+    {
+        void OnNotify(T subject);
     }
     public interface IObserver
     {
-        void Update(ISubject subject);
+        void OnNotify(IObserver Myself);
     }
-
+    public interface ISubject<T>
+    {
+        void Notify(T data);
+        void Subscribe(IObserver<T> observer);
+        void UnSubscribe(IObserver<T> observer);
+    }  
     public interface ISubject
     {
-   
-        void Notify();
+        void Notify(IObserver observer);
         void Subscribe(IObserver observer);
         void UnSubscribe(IObserver observer);
     }
+
+
 }
---
+++ Assets/Scripts/DesignPatterns/ObserverSO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DesignPattern
+{
+    [CreateAssetMenu (fileName = "Observer", menuName = "ScriptableObjects/DesignPattern/Observer")]
+    public class ObserverSO : ScriptableObject, ISubject
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+        public void Notify(IObserver theOneWhoDidIt)
+        {
+            for (int i = 0; i < _observers.Count; i++)
+                _observers[i].OnNotify(theOneWhoDidIt);
+        }
+
+        public void Subscribe(IObserver observer)
+        {
+            if (!_observers.Contains(observer))
+                _observers.Add(observer);
+        }
+
+        public void UnSubscribe(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+    }
+
+
+}
