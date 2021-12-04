d45216b449b911f7147713b00691f5efb637eae5
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-12-05 00:54:17 +0200

    VS Screen

---
+++ Assets/Scripts/Loading Scene/VSScreen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class VSScreen : MonoBehaviour
+{
+    [SerializeField]
+    Animator _vsAnimator;
+    [SerializeField]
+    TextMeshProUGUI _playerNameText;
+    [SerializeField]
+    TextMeshProUGUI _opponentNameText;
+    int _playVSHash = Animator.StringToHash("PlayVSAnim");
+    private void Start()
+    {
+         StartVSScreen();
+    }
+
+    public void StartVSScreen()
+    {
+        //names
+        if (Battles.BattleData.Player != null || Battles.BattleData.Opponent != null)
+        {
+            _playerNameText.text = Battles.BattleData.Player.CharacterData.Info.CharacterName;
+            _opponentNameText.text = Battles.BattleData.Opponent.CharacterData.Info.CharacterName;
+        }
+        StartAnimation();
+    }
+    void StartAnimation()
+    {
+        _vsAnimator.SetTrigger(_playVSHash);
+    }
+}
