5ced6a7530e4aa0ebaae28eb9dc0374019a037ec
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-12-08 15:55:04 +0200

    Credits

---
+++ Assets/Scripts/Meta Game/HyperlinksSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+[CreateAssetMenu(fileName = "Hyperlinks", menuName = "ScriptableObjects/Hyperlinks")]
+public class HyperlinksSO : ScriptableObject
+{
+    [SerializeField] [InfoBox("0 = Discord")]
+    string[] _links;
+    public void UseLink(int linkNumber)
+    {
+        Application.OpenURL(_links[linkNumber]);
+    }
+
+}
