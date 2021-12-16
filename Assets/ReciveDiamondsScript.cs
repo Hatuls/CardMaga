939a23d5b433c34ac6191cc2df71843c962aabd9
EDeyal <75429849+EDeyal@users.noreply.github.com>
2021-12-16 17:53:33 +0200

    Rewards screen

---
+++ Assets/ReciveDiamondsScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ReciveDiamondsScript : MonoBehaviour
+{
+    [SerializeField]
+    int _diamondAmount;
+    [SerializeField]
+    TextMeshProUGUI _diamondAmountText;
+
+
+
+    public void OnReciveRewards()
+    {
+
+    }
+
+        
+}
