59b68eb481d399dc7c0ca57a61356c907deb1127
Rei <reisegev@yahoo.com>
2021-12-17 22:10:04 +0200

    banks

--- Assets/Scripts/Battle Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Battle Scripts/Managers/AudioManager.cs
@@ -36,10 +36,10 @@ public class AudioManager : MonoBehaviour
             DontDestroyOnLoad(this.gameObject);
     }
 
-   
+
     private void OnDisable()
     {
-        
+
         SceneHandler.onFinishLoadingScene -= SceneParameter;
     }
     private void FmodInit()
@@ -122,7 +122,7 @@ public class AudioManager : MonoBehaviour
         {
             foreach (var fmodData in _fmodLibrary)
                 fmodData.StopSound(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-            
+
             _fmodLibrary.Clear();
         }
     }
@@ -135,10 +135,10 @@ public class AudioManager : MonoBehaviour
             case SceneHandler.ScenesEnum.LoadingScene:
             case SceneHandler.ScenesEnum.MainMenuScene:
             case SceneHandler.ScenesEnum.MapScene:
-               // FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Scene Parameter",  0  );
+                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Scene Parameter", 0);
                 break;
             case SceneHandler.ScenesEnum.GameBattleScene:
-              //  FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Scene Parameter",2 );
+                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Scene Parameter", 1);
                 break;
             default:
                 break;
