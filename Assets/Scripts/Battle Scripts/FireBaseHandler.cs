4a58863934d610dc751a383ba4d61e5f1a0550db
Rei <reisegev@yahoo.com>
2021-12-26 20:45:03 +0200

    firebase

---
+++ Assets/Scripts/Battle Scripts/FireBaseHandler.cs
@@ -0,0 +1,26 @@
+﻿using Firebase;
+using Firebase.Analytics;
+//#if !UNITY_EDITOR
+public static class FireBaseHandler
+{
+    public static void Init()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }  
+
+    public static void SendEvent(string eventName, params Parameter[] parameters)
+    {
+        FirebaseAnalytics.LogEvent(eventName, parameters);
+    }
+
+    public static void SendEvent(string eventName)
+    {
+        FirebaseAnalytics.LogEvent(eventName);
+    }
+
+
+}
+//#endif
