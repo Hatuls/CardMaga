dbade522b217d49fff50de73688a6724539f5d7b
Rei <reisegev@yahoo.com>
2022-01-03 01:05:01 +0200

    Update ReturnLoadingScene.cs

--- Assets/Scripts/Meta Game/ReturnLoadingScene.cs
+++ Assets/Scripts/Meta Game/ReturnLoadingScene.cs
@@ -20,10 +20,19 @@ public class ReturnLoadingScene : MonoBehaviour
                 break;
             case SceneHandler.ScenesEnum.GameBattleScene:
                 if (battledata.Player != null &&
-                    battledata.Player.CharacterData != null && 
+                    battledata.Player.CharacterData != null &&
                     battledata.Opponent != null &&
                     battledata.Opponent.CharacterData != null)
                     OpenPanel();
+                else if (battledata.Player != null &&
+                    battledata.Player.CharacterData != null &&
+                    battledata.Map != null)
+                {
+                    SceneHandler.LoadScene(SceneHandler.ScenesEnum.MapScene);
+                    Close();
+                }
+
+
                 break;
             default:
                 break;
