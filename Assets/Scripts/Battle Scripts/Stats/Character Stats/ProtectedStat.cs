451ec67e2850fc2cf4e16208102be9ffe1b3f5d4
Hatuls <64154408+Hatuls@users.noreply.github.com>
2022-01-15 00:48:11 +0200

    vulnerable vfx

--- Assets/Scripts/Battle Scripts/Stats/Character Stats/ProtectedStat.cs
+++ Assets/Scripts/Battle Scripts/Stats/Character Stats/ProtectedStat.cs
@@ -14,12 +14,48 @@ namespace Characters.Stats
     public class WeakStat : StatAbst
     {
         VFXController _vfxController;
-        ParticleSystemVFX _weakParticleVFX;
+        ParticleSystemVFX _vulnerableVFX;
         public WeakStat(bool isPlayer, int amount) : base(isPlayer, amount)
         {
         }
 
         public override KeywordTypeEnum Keyword => KeywordTypeEnum.Weak;
+        public override void Add(int amount)
+        {
+            if (Amount == 0)
+            {
+                if (_vulnerableVFX == null)
+                {
+                    var tuffle = VFXManager.Instance.RecieveParticleSystemVFX(isPlayer, Factory.GameFactory.Instance.KeywordSOHandler.GetKeywordSO(Keyword).GetVFX());
+                    _vulnerableVFX = tuffle.Item1;
+                    _vulnerableVFX.Cancel();
+                    _vfxController = tuffle.Item2;
+                }
+                _vulnerableVFX.StartVFX(_vulnerableVFX.VFXID, _vfxController.AvatarHandler.GetBodyPart(_vulnerableVFX.VFXID.DefaultBodyPart));
+            }
+            base.Add(amount);
+        }
+        public override void Reduce(int amount)
+        {
+            base.Reduce(amount);
+            if (Amount <= 0 && _vulnerableVFX.IsPlaying)
+            {
+                _vulnerableVFX.Cancel();
+            }
+        }
+        public override void Reset(int value = 0)
+        {
+            base.Reset(value);
+            _vulnerableVFX?.Cancel();
+        }
+    }
+    public class VulnerableKeyword : StatAbst
+    {
+        VFXController _vfxController;
+        ParticleSystemVFX _weakParticleVFX;
+        public VulnerableKeyword(bool isPlayer, int amount) : base(isPlayer, amount)
+        {
+        }
         public override void Add(int amount)
         {
             if (Amount == 0)
@@ -48,13 +84,6 @@ namespace Characters.Stats
             base.Reset(value);
             _weakParticleVFX?.Cancel();
         }
-    }
-    public class VulnerableKeyword : StatAbst
-    {
-        public VulnerableKeyword(bool isPlayer, int amount) : base(isPlayer, amount)
-        {
-        }
-
         public override KeywordTypeEnum Keyword => KeywordTypeEnum.Vulnerable;
     }
 }
