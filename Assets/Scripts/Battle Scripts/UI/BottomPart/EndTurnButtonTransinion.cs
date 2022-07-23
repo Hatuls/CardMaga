ecb3eeac0a6eb157b4830c836c4c8d18e6203096
ליעד חלוצי <liadhalotzi@gmail.com>
2022-07-23 23:29:25 +0300

    Stamina animations

--- Assets/Scripts/Battle Scripts/UI/BottomPart/EndTurnButtonTransinion.cs
+++ Assets/Scripts/Battle Scripts/UI/BottomPart/EndTurnButtonTransinion.cs
@@ -6,35 +6,34 @@ using Sirenix.OdinInspector;
 public class EndTurnButtonTransinion: MonoBehaviour
 {
     [Header("Fields")]
-    [SerializeField] TransitionPackSO _transitionPackSO;
+    [SerializeField] private TransitionPackSO _transitionPackSO;
     
     //Transform of the UI.
-    [SerializeField] RectTransform _currentRectTransform;
-    [SerializeField] RectTransform _destinationRectTransform;
+    [SerializeField] private RectTransform _currentRectTransform;
+    [SerializeField] private RectTransform _destinationRectTransform;
 
     //Scrip of thransitions. Recive Transision pack SO
-    private RectTransitionManager endTurnTransitionManager;
+    private RectTransitionManager _endTurnTransitionManager;
 
     void Start()
     {
-        endTurnTransitionManager = new RectTransitionManager(_currentRectTransform);
+        _endTurnTransitionManager = new RectTransitionManager(_currentRectTransform);
         if (_transitionPackSO != null)
         {
-            endTurnTransitionManager = new RectTransitionManager(_currentRectTransform);
+            _endTurnTransitionManager = new RectTransitionManager(_currentRectTransform);
         }
     }
 
     [Button]
     public void Scale()
     {
-        endTurnTransitionManager.Scale(_transitionPackSO);
+        _endTurnTransitionManager.Scale(_transitionPackSO);
     }
 
     [Button]
     public void Transition()
     {
-        endTurnTransitionManager.Transition(_destinationRectTransform, _transitionPackSO);
-
+        _endTurnTransitionManager.Transition(_destinationRectTransform, _transitionPackSO);
     }
 
 }
