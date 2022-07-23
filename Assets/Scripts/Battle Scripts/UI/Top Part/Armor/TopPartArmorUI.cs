6cd0854ab24b79a6e208ad9703038363abcd943e
Jonnix12 <jonharel48@gmail.com>
2022-07-23 15:38:32 +0300

    Add TestUI Scene

---
+++ Assets/Scripts/Battle Scripts/UI/Top Part/Armor/TopPartArmorUI.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace CardMaga.UI.Visuals
+{
+    public class TopPartArmorUI : MonoBehaviour
+    {
+        [SerializeField] TransitionPackSO _gainArmorTransitionSO;
+        [SerializeField] TransitionPackSO _reduceArmorTransitionSO;
+        [SerializeField] TransitionPackSO _breakArmorTransitionSO;
+        [SerializeField] TextMeshProUGUI _armorAmountText;
+        [SerializeField] Image _armorImage;
+        [SerializeField] Image _breakArmorImage;
+
+        RectTransitionManager _armorManager;
+        RectTransitionManager _breakArmorManager;
+        int _currentArmor;
+        bool _canArmorReset = true;
+        bool _isReducedByHalf = false;
+        public bool IsArmorResets { set => _canArmorReset = value; }
+        public bool IsReducedByHalf { set => _isReducedByHalf = value; }
+
+        private void Awake()
+        {
+            
+        }
+        private void Start()
+        {
+            _armorManager = new RectTransitionManager(_armorImage.rectTransform);
+            _breakArmorManager = new RectTransitionManager(_breakArmorImage.rectTransform);
+        }
+        public void SetArmor(int amount)
+        {
+            if (amount > _currentArmor)
+            {
+                GainArmor(amount);
+            }
+            else
+            {
+                ReduceArmor(amount);
+            }
+        }
+        public void ResetArmor()
+        {
+            if (_currentArmor == 0)
+            {
+                Debug.Log("Armor is already 0 no need to change it");
+                return;
+            }
+            if (_canArmorReset)
+            {
+                _currentArmor = 0;
+            }
+            else if (_isReducedByHalf)
+            {
+                float half = _currentArmor / 2;
+
+                _currentArmor = (int)Mathf.Ceil(half);
+            }
+            else
+            {
+                Debug.Log("Armor should not reset");
+            }
+        }
+        private void GainArmor(int amount)
+        {
+            if (_currentArmor == 0)
+            {
+                ActivateArmor();
+            }
+            _currentArmor = amount;
+            SetText(_currentArmor);
+        }
+        private void ReduceArmor(int amount)
+        {
+            var armorDelta = _currentArmor - amount;
+            if (armorDelta <= 0)
+            {
+                _currentArmor = 0;
+                //we have 0 or less armor
+                BreakArmorAnimation();
+                DeactivateArmor();
+            }
+            else
+            {
+                _currentArmor = armorDelta;
+            }
+            ReduceArmorAnimation();
+        }
+        private void ReduceArmorAnimation()
+        {
+            _armorManager.Transition(_armorImage.transform.position, _reduceArmorTransitionSO);
+        }
+        private void GainArmorAnimation()
+        {
+            _armorManager.Transition(_armorImage.transform.position, _gainArmorTransitionSO);
+        }
+        private void SetText(int amount)
+        {
+            _armorAmountText.text = string.Concat(amount);
+        }
+        private void ActivateArmor()
+        {
+            //need implementation
+        }
+        private void DeactivateArmor()
+        {
+            //need implementation
+        }
+        private void BreakArmorAnimation()
+        {
+            //shatter animation
+        }
+    }
+}
