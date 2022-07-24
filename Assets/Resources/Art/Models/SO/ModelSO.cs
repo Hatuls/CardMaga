253e7b5af971e09cc51408a2baeece2ce4b2dceb
Rei <reisegev@yahoo.com>
2022-07-25 00:40:54 +0300

    Model SO

---
+++ Assets/Resources/Art/Models/SO/ModelSO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Model SO", menuName = "ScriptableObjects/Art/Models/New Model SO")]
+public class ModelSO : ScriptableObject
+{
+    public GameObject Model;
+    public MaterialsForModel[] Materials;
+}
+
+[Serializable]
+public class MaterialsForModel
+{
+    public Material Normal;
+    public Material Tinted;
+} 
