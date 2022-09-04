9811a685433d2f870221efb4cf14e804cec9c821
EDeyal <75429849+EDeyal@users.noreply.github.com>
2022-09-04 16:59:52 +0300

    Start of Combo visual Handler scripts

---
+++ Assets/Scripts/UI/Visuals/Combos/BaseComboVisualHandler.cs
@@ -0,0 +1,10 @@
+﻿using Battle.Combo;
+
+namespace CardMaga.UI
+{
+    public abstract class BaseComboVisualHandler : BaseVisualHandler<Combo>
+    {
+        //#combo visual Assigner
+        //#combo Text Assigner
+    }
+}
