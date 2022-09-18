005a449d9e625a4110962a554ff2afba3d8eb3da
Rei <reisegev@yahoo.com>
2022-09-18 21:44:39 +0300

    Tracker

---
+++ Assets/Scripts/Tracking Objects/TrackerID.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+namespace CardMaga.Trackers
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/Tracker/New Tracker ID", fileName = "New Tracker ID")]
+    public class TrackerID : ScriptableObject
+    {
+
+    }
+}
