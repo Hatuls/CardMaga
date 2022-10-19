ae988a90a48719367dd288fa9fb2a7746ad33439
Jonnix12 <jonharel48@gmail.com>
2022-10-19 09:46:37 +0300

    Update

---
+++ Assets/Scripts/ObjectPool/ComboUIPool.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Battle.Combo;
+using UnityEngine;
+
+public class ComboUIPool : MonoBehaviour
+{
+    [SerializeField] private ComboUI _comboUIPrefab;
+    [SerializeField] private RectTransform _parent;
+
+    private ObjectPool<ComboUI> _cardUiPool;
+
+    private void Awake()
+    {
+        _cardUiPool = new ObjectPool<ComboUI>(_comboUIPrefab, _parent);
+    }
+
+    public List<ComboUI> GetCardUIs(params Combo[] comboData)
+    {
+        if (comboData == null || comboData.Length == 0)
+            return null;
+            
+        List<ComboUI> output = new List<ComboUI>(comboData.Length);
+
+        for (int i = 0; i < comboData.Length; i++)
+        {
+            ComboUI cache = _cardUiPool.Pull();
+            
+            cache.AssignComboData(comboData[i]);
+            
+            output.Add(cache);
+        }
+
+        return output;
+    }
+}
---
+++ Assets/Scripts/UI/Combos/ComboUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Battle.Combo;
+using CardMaga.UI.Combos;
+using CardMaga.UI.ScrollPanel;
+using UnityEngine;
+
+public class ComboUI : MonoBehaviour , IShowableUI , IPoolable<ComboUI>
+{
+    public event Action<ComboUI> OnDisposed;
+    
+    [SerializeField] private ComboVisualHandler _comboVisual;
+    
+    public void AssignComboData(Combo comboData)
+    {
+        _comboVisual.Init(comboData);
+    }
+    
+    public void Dispose()
+    {
+        _comboVisual.Dispose();
+    }
+
+    public void Show()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        gameObject.SetActive(true);
+    }
+}
