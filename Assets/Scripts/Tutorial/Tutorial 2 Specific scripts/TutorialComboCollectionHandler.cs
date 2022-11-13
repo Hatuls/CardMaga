d5c6fa08624be0372d947ab4feb77dae1ca2f5b2
Liad Halotzi <liadhalotzi@gmail.com>
2022-11-13 15:12:05 +0200

    More progress on tutorial 2

--- Assets/Scripts/Tutorial/Tutorial 2 Specific scripts/TutorialComboCollectionHandler.cs
+++ Assets/Scripts/Tutorial/Tutorial 2 Specific scripts/TutorialComboCollectionHandler.cs
@@ -1,48 +1,56 @@
 ﻿using CardMaga.Trackers;
 using ReiTools.TokenMachine;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using CardMaga.Battle.UI;
 
-public class TutorialComboCollectionHandler : MonoBehaviour
+namespace CardMaga.UI.Collections
 {
-    [SerializeField] TrackerID trackerID;
-    [SerializeField] private DialoguesFlow _dialoguesFlow5;
-    [SerializeField] private UnityEvent OnCollectionButtonLoadOnPanel;
-    [SerializeField] private UnityEvent OnCollectionButtonExitPanel;
-    
-    private IDisposable _token;
-    private RectTransform _enterButtonTransform;
-
-   
-    public void LoadButtonOnPanel(ITokenReciever tokenReciever)
+    public class TutorialComboCollectionHandler : MonoBehaviour
     {
-        _token = tokenReciever.GetToken();
-        _dialoguesFlow5.gameObject.SetActive(true);
-        if (OnCollectionButtonLoadOnPanel != null)
-            OnCollectionButtonLoadOnPanel.Invoke();
-        _enterButtonTransform = TrackerHandler.GetTracker(trackerID).RectTransform;
-        TutorialClickHelper.Instance.LoadObject(true, true, null, _enterButtonTransform);
-        ComboCollectorTutorialTapDetector.OnButtonPress += AfterEnterButtonPress;
-    }
+        [SerializeField] TrackerID trackerID;
+        [SerializeField] private DialoguesFlow _dialoguesFlow5;
+        [SerializeField] private UnityEvent OnCollectionButtonLoadOnPanel;
+        [SerializeField] private UnityEvent OnCollectionButtonExitPanel;
 
-    private void AfterEnterButtonPress()
-    {
-        if (OnCollectionButtonExitPanel != null)
-            OnCollectionButtonExitPanel.Invoke();
-        _dialoguesFlow5.gameObject.SetActive(false);
-        ComboCollectorTutorialTapDetector.OnButtonPress -= AfterEnterButtonPress;
-        ReleaseToken();
-    }
+        private IDisposable _token;
+        private RectTransform _enterButtonTransform;
 
-    private void ReleaseToken()
-    {
-        if (_token != null)
-            _token.Dispose();
 
-        else
-            Debug.LogError("No token to release");
+        public void LoadButtonOnPanel(ITokenReciever tokenReciever)
+        {
+            _token = tokenReciever.GetToken();
+            _dialoguesFlow5.gameObject.SetActive(true);
+            if (OnCollectionButtonLoadOnPanel != null)
+                OnCollectionButtonLoadOnPanel.Invoke();
+            _enterButtonTransform = TrackerHandler.GetTracker(trackerID).RectTransform;
+            TutorialClickHelper.Instance.LoadObject(true, true, null, _enterButtonTransform);
+            ComboCollectorTutorialTapDetector.OnButtonPress += AfterEnterButtonPress;
+        }
+
+        private void AfterEnterButtonPress()
+        {
+            if (OnCollectionButtonExitPanel != null)
+                OnCollectionButtonExitPanel.Invoke();
+            _dialoguesFlow5.gameObject.SetActive(false);
+            ComboCollectorTutorialTapDetector.OnButtonPress -= AfterEnterButtonPress;
+            ReleaseToken();
+        }
+
+        public void AfterCollectioOpen()
+        {
+            //BattleUiManager.Instance.ComboAndDeckCollectionBattleHandler;
+        }
+
+        private void ReleaseToken()
+        {
+            if (_token != null)
+                _token.Dispose();
+
+            else
+                Debug.LogError("No token to release");
+        }
     }
 }
+
