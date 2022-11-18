fcf884aad8e4377b72d32f5988559cbc874f55e9
EDeyal <75429849+EDeyal@users.noreply.github.com>
2022-11-18 21:47:24 +0200

    Start of Account Sripts

---
+++ Assets/Scripts/UI/Visuals/AccountBar/AccountBarVisualData.cs
@@ -0,0 +1,22 @@
+﻿namespace CardMaga.UI.Visuals
+{
+    public class AccountBarVisualData
+    {
+        #region Fields
+        string _accountNickname;
+        int _accountImageID;
+        int _currentExpAmount;
+        int _maxExpAmount;
+        int _accountLevel;
+        #endregion
+
+        #region Properties
+        public string AccountNickname => _accountNickname;
+        public int AccountImageID => _accountImageID;
+        public int CurrentExpAmount => _currentExpAmount;
+        public int MaxExpAmount => _maxExpAmount;
+        public int AccountLevel => _accountLevel;
+        #endregion
+        //Currency Types
+    }
+}
--- Assets/Scripts/UI/Visuals/Assigners/Text/BaseTextAssigner.cs
+++ Assets/Scripts/UI/Visuals/Assigners/Text/BaseTextAssigner.cs
@@ -3,6 +3,6 @@
     [System.Serializable]
     public abstract class BaseTextAssigner<T> : BaseVisualHandler<T>
     {
-
+        protected const string EMPTY_TEXT = "";
     }
 }
