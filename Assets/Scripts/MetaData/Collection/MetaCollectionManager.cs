6cde8962c7789e9636c99093a14dc35c5662e109
Jonnix12 <jonharel48@gmail.com>
2022-11-21 11:41:41 +0200

    Update

--- Assets/Scripts/MetaData/Collection/MetaCollectionManager.cs
+++ Assets/Scripts/MetaData/Collection/MetaCollectionManager.cs
@@ -12,7 +12,7 @@ namespace CardMaga.MetaData.Collection
         [SerializeField] private UnityEvent OnSuccessUpdateDeck;
         [SerializeField] private UnityEvent OnFailedUpdateDeck;
 
-        [SerializeField] private TMP_InputField _deckName;
+        [SerializeField] private InputFieldHandler _deckName;
         [SerializeField] private AccountDataAccess _accountDataAccess;
         [SerializeField] private MetaUICollectionManager _metaUICollectionManager;
         private AccountDataCollectionHelper _accountDataCollectionHelper;
@@ -25,8 +25,7 @@ namespace CardMaga.MetaData.Collection
             _deckBuilder = new DeckBuilder(_accountDataCollectionHelper);
             _deckBuilder.AssingDeckToEdit(_accountDataAccess.AccountData.CharacterDatas.CharacterData.Decks[0]);
 
-
-            _deckName.onValueChanged.AddListener(EditDeckName);//need to ask rei
+            _deckName.OnValueChange += EditDeckName;
             _deckBuilder.OnSuccessCardAdd += _metaUICollectionManager.AddCardUI;
             _deckBuilder.OnSuccessCardRemove += _metaUICollectionManager.RemoveCardUI;
             _deckBuilder.OnSuccessComboAdd += _metaUICollectionManager.AddComboUI;
@@ -56,6 +55,7 @@ namespace CardMaga.MetaData.Collection
 
         private void OnDestroy()
         {
+            _deckName.OnValueChange -= EditDeckName;
             _deckBuilder.OnSuccessCardAdd -= _metaUICollectionManager.AddCardUI;
             _deckBuilder.OnSuccessCardRemove -= _metaUICollectionManager.RemoveCardUI;
             _deckBuilder.OnSuccessComboAdd -= _metaUICollectionManager.AddComboUI;
