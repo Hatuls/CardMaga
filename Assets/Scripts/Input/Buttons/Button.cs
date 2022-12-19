34446481b26c740f5bcaecfa93eeba054d6a4fa7
Rei <reisegev@yahoo.com>
2022-12-19 22:17:32 +0200

    Replaces buttons in sprite atlas

--- Assets/Scripts/Input/Buttons/Button.cs
+++ Assets/Scripts/Input/Buttons/Button.cs
@@ -9,15 +9,18 @@ namespace CardMaga.Input
     {
         [SerializeField] protected TMP_Text _buttonText;
         [SerializeField] protected Image _renderer;
+
         [Header("Sprites")]
         [SerializeField] protected Sprite _onPress;
+
+        [OnValueChanged("ChangeToIdle")]
         [SerializeField] protected Sprite _onIdle;
 
         protected override void PointDown()
         {
             if (_onPress == null)
                 return;
-            
+
             _renderer.sprite = _onPress;
             base.PointDown();
         }
@@ -25,25 +28,25 @@ namespace CardMaga.Input
         {
             if (_onIdle == null)
                 return;
-            
+
             _renderer.sprite = _onIdle;
             base.PointUp();
         }
-        
+
         [Button("Toggle Text")]
         public void ToggleActiveState()
         {
             if (_buttonText == null)
                 return;
-            
+
             _buttonText.gameObject.SetActive(!_buttonText.gameObject.activeSelf);
         }
 
         public void SetButtonText(string text)
         {
-            if (_buttonText == null)
+            if (_buttonText == null || _buttonText.text.Equals(text))
                 return;
-            
+
             _buttonText.text = text;
         }
 
@@ -54,7 +57,12 @@ namespace CardMaga.Input
         [OnValueChanged("UpdateText")]
         public string Text;
         private void UpdateText() => SetButtonText(Text);
+        private void ChangeToIdle()
+        {
+            if (_renderer != null)
+                _renderer.sprite = _onIdle;
+        }
 #endif
-    }    
+    }
 }
 
