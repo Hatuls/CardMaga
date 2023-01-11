0cf1977886036b28f43dc02cc82123d83b9af25d
Jonnix12 <jonharel48@gmail.com>
2023-01-11 15:35:48 +0200

    Update to the main manu

---
+++ Assets/Scripts/MainMenu/DiscordButtonAnimtion/DiscordButtonAnimtionHandler.cs
@@ -0,0 +1,33 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using Random = System.Random;
+
+public class DiscordButtonAnimtionHandler : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    [SerializeField, MinMaxSlider(1,20,true)] private Vector2 _timeToDely;
+    private float _timeToWait;
+    private float _delay;
+    
+    
+    private void FixedUpdate()
+    {
+        if (_delay > 0)
+        {
+            _delay -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            _animator.SetTrigger("StartAnimation");
+            RestTimer();
+        }
+    }
+
+    private void RestTimer()
+    {
+        var random = new Random();
+
+        _delay = random.Next(Mathf.RoundToInt(_timeToDely.x), Mathf.RoundToInt(_timeToDely.y));
+    }
+}
