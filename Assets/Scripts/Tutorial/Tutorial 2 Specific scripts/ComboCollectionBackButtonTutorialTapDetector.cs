60bf9352f422b2d3558a40e56d26aa55aaaf388d
Rei <reisegev@yahoo.com>
2023-01-12 17:05:35 +0200

    Fixed tutorial 2

--- Assets/Scripts/Tutorial/Tutorial 2 Specific scripts/ComboCollectionBackButtonTutorialTapDetector.cs
+++ Assets/Scripts/Tutorial/Tutorial 2 Specific scripts/ComboCollectionBackButtonTutorialTapDetector.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ComboCollectorBackButtonTutorialTapDetector : MonoBehaviour
+public class ComboCollectionBackButtonTutorialTapDetector : MonoBehaviour
 {
     [SerializeField] Button _comboCollectionExitButton;
 
