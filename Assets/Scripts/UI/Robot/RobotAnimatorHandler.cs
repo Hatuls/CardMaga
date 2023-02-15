63188130cfe4a170bcb8554745e49a64e46468e2
EDeyal <75429849+EDeyal@users.noreply.github.com>
2023-02-15 11:06:13 +0200

    Robot Animator Handler

---
+++ Assets/Scripts/UI/Robot/RobotAnimatorHandler.cs
@@ -0,0 +1,52 @@
+﻿using CardMaga.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+enum RobotEarAnimationType
+{
+    Idle,
+    ZigZag,
+    Wiggle,
+    SmallToBig,
+    Wave
+}
+public class RobotAnimatorHandler : MonoBehaviour,ICheckValidation
+{
+    [SerializeField] Animator _earAnimator;
+    [SerializeField] RobotEarAnimationType _earType;
+    [SerializeField] RobotEyeSO _eyeType;
+    [SerializeField] Image _eyeImage;
+
+    private void Start()
+    {
+        CheckValidation();
+        _earAnimator.SetTrigger(GetAnimationTrigger());
+        _eyeImage.sprite = _eyeType.EyeSprite;
+    }
+    private string GetAnimationTrigger()
+    {
+        switch (_earType)
+        {
+            case RobotEarAnimationType.Idle:
+                return "Idle";
+            case RobotEarAnimationType.ZigZag:
+                return "ZigZag";
+            case RobotEarAnimationType.Wiggle:
+                return "Wiggle";
+            case RobotEarAnimationType.SmallToBig:
+                return "SmallToBig";
+            case RobotEarAnimationType.Wave:
+                return "Wave";
+            default:
+                return null;
+        }
+    }
+
+    public void CheckValidation()
+    {
+        if (!_eyeImage)
+        {
+            Debug.LogError("Robot animator handler has no eye image");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Robot/RobotEyeSO.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "New Robot Eye Type",menuName = "ScriptableObjects/Robot/EyeTypeSO")]
+public class RobotEyeSO : ScriptableObject
+{
+    public Sprite EyeSprite;
+}
